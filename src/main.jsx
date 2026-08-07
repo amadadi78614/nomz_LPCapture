d88f9164a0a7b841e3527b3e238ff6b20ae7f3d8
amadadi78614 <mohamed.nomani@gmail.com>
2026-08-07 06:32:53 +0200

    Load six-round regular-season correction before rendering

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './data/legacyRound3Update';
 import './data/franchiseRound5Update';
 import './data/round5SiteSync';
 import './data/franchiseFinalsRunUpdate';
+import './data/regularSeasonSixRoundFix';
 import './data/ladiesSeason2Data';
 import './data/ladiesSeason2HomeSync';
 import './data/ladiesSeason2LeagueSync';

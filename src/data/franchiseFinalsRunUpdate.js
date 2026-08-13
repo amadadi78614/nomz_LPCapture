d3942084d2bc420044bc798d23a5325a20dcce41
amadadi78614 <mohamed.nomani@gmail.com>
2026-08-13 23:02:07 +0200

    Add semifinal and correct finals player mappings

--- src/data/franchiseFinalsRunUpdate.js
+++ src/data/franchiseFinalsRunUpdate.js
@@ -27,52 +27,64 @@ export const FINALS_RUN_FIXTURES = [
     start: '2026-08-03T18:00:00+02:00', status: 'final', court: 'Play 360',
     score: { winner: 'home', totals: [13, 7], rubberWins: [4, 2], rubbers: [
       rubber({ slot: '18:00', court: 'P1', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Cian Maritz', 'Yusuf Packery'], awayNames: ['Uwaiz Patel', 'Yusuf Patel'], sets: [[3,6],[6,3],[11,13]], games: [0,3], winner: 'away' }),
-      rubber({ slot: '20:30', court: 'P1', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Pieter Badenhorst', 'Justin van Staaden'], awayNames: ['Reinhardt van Wyk', 'Ahmed Mungalee'], sets: [[1,6],[6,3],[10,5]], games: [3,0], winner: 'home' }),
-      rubber({ slot: '19:15', court: 'P2', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Adil Patel', 'Sulaiman Packery'], awayNames: ['Ray Cassim', 'Ebrahim Mungalee'], sets: [[6,2],[6,3],[10,4]], games: [4,0], winner: 'home' }),
+      rubber({ slot: '20:30', court: 'P1', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Pieter Badenhorst', 'Justin van Staaden'], awayNames: ['Reinhardt Trollip', 'Ahmed Ismail'], sets: [[1,6],[6,3],[10,5]], games: [3,0], winner: 'home' }),
+      rubber({ slot: '19:15', court: 'P2', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Adil Patel', 'Suliman Patel'], awayNames: ['Rayhaan Dinath', 'Ebrahim Mungalee'], sets: [[6,2],[6,3],[10,4]], games: [4,0], winner: 'home' }),
       rubber({ slot: '20:30', court: 'P2', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Naeem Omar', 'Alfaiz Mamji'], awayNames: ['Warno Smit', 'Fahad Patel'], sets: [[6,2],[2,6],[10,4]], games: [3,0], winner: 'home' }),
-      rubber({ slot: '18:00', court: 'P3', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Majid Bapu', 'Imtiaz Mohamed'], awayNames: ['Joe', 'Danie'], sets: [[1,6],[1,6],[4,10]], games: [0,4], winner: 'away' }),
-      rubber({ slot: '19:15', court: 'P3', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Drikus', 'Warren Morgan'], awayNames: ['Jacques', 'Zakeem'], sets: [[6,1],[5,7],[10,7]], games: [3,0], winner: 'home' }),
+      rubber({ slot: '18:00', court: 'P3', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Majid Bapu', 'Imtiaz Mohamed'], awayNames: ['Jude van den Berg', 'Danie Rautenbach'], sets: [[1,6],[1,6],[4,10]], games: [0,4], winner: 'away' }),
+      rubber({ slot: '19:15', court: 'P3', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Drikus Prins', 'Warren Morgan'], awayNames: ['Jacques Burger', 'Zaeem Sadiq'], sets: [[6,1],[5,7],[10,7]], games: [3,0], winner: 'home' }),
     ] },
   },
   {
     id: 'fx-w6-kicksmashers-ice', round: 6, league: 'mens', home: 'samurai-kicksmashers', away: 'ice-breakers',
     start: '2026-08-03T18:00:00+02:00', status: 'final', court: 'Padel 24',
     score: { winner: 'away', totals: [4, 17], rubberWins: [1, 5], rubbers: [
-      rubber({ slot: '18:00', court: 'P1', homeFranchise: 'samurai-kicksmashers', awayFranchise: 'ice-breakers', homeNames: ['Durell Pillay', 'Bryan Theron'], awayNames: ['Duhan Swart', 'Imaad'], sets: [[2,6],[6,7],[4,10]], games: [0,4], winner: 'away' }),
-      rubber({ slot: '18:00', court: 'P3', homeFranchise: 'samurai-kicksmashers', awayFranchise: 'ice-breakers', homeNames: ['Imraan', 'Nuaym Shaik'], awayNames: ['Waldo van Tonder', 'Shaun Caromba'], sets: [[5,7],[3,6],[10,4]], games: [0,3], winner: 'away' }),
-      rubber({ slot: '19:15', court: 'P2', homeFranchise: 'samurai-kicksmashers', awayFranchise: 'ice-breakers', homeNames: ['Sikander Cassim', 'Tim'], awayNames: ['Zahid Methar', 'Chaz Taylor'], sets: [[6,4],[6,7],[9,11]], games: [0,3], winner: 'away' }),
-      rubber({ slot: '19:15', court: 'P3', homeFranchise: 'samurai-kicksmashers', awayFranchise: 'ice-breakers', homeNames: ['Brent', 'Riaz Ahmed Bellim'], awayNames: ['Wayne Enslin', 'Sergio'], sets: [[6,3],[6,4],[10,8]], games: [4,0], winner: 'home' }),
-      rubber({ slot: '20:30', court: 'P1', homeFranchise: 'samurai-kicksmashers', awayFranchise: 'ice-breakers', homeNames: ['Siraaj Shaik', 'Danyaal'], awayNames: ['Zaheer Methar', 'Maaz Randera'], sets: [[3,6],[6,4],[5,10]], games: [0,3], winner: 'away' }),
-      rubber({ slot: '20:30', court: 'P3', homeFranchise: 'samurai-kicksmashers', awayFranchise: 'ice-breakers', homeNames: ['Muneer Shaik', 'Shaun Moropa'], awayNames: ['Jacques', 'Phil-Mar Van Rensburg'], sets: [[1,6],[3,6],[7,10]], games: [0,4], winner: 'away' }),
+      rubber({ slot: '18:00', court: 'P1', homeFranchise: 'samurai-kicksmashers', awayFranchise: 'ice-breakers', homeNames: ['Durell Pillay', 'Bryan Theron'], awayNames: ['Duhan Swart', 'Imaad Arabi'], sets: [[2,6],[6,7],[4,10]], games: [0,4], winner: 'away' }),
+      rubber({ slot: '18:00', court: 'P3', homeFranchise: 'samurai-kicksmashers', awayFranchise: 'ice-breakers', homeNames: ['Imraan Khan', 'Nuaym Shaik'], awayNames: ['Waldo van Tonder', 'Shaun Caromba'], sets: [[5,7],[3,6],[10,4]], games: [0,3], winner: 'away' }),
+      rubber({ slot: '19:15', court: 'P2', homeFranchise: 'samurai-kicksmashers', awayFranchise: 'ice-breakers', homeNames: ['Sikander Cassim', 'Tim Forssman'], awayNames: ['Zahid Methar', 'Chaz Taylor'], sets: [[6,4],[6,7],[9,11]], games: [0,3], winner: 'away' }),
+      rubber({ slot: '19:15', court: 'P3', homeFranchise: 'samurai-kicksmashers', awayFranchise: 'ice-breakers', homeNames: ['Brent Grix', 'Riaz Ahmed Bellim'], awayNames: ['Wayne Enslin', 'Sergio Correia'], sets: [[6,3],[6,4],[10,8]], games: [4,0], winner: 'home' }),
+      rubber({ slot: '20:30', court: 'P1', homeFranchise: 'samurai-kicksmashers', awayFranchise: 'ice-breakers', homeNames: ['Siraaj Shaik', 'Danyaal Nomani'], awayNames: ['Zaheer Methar', 'Maaz Randera'], sets: [[3,6],[6,4],[5,10]], games: [0,3], winner: 'away' }),
+      rubber({ slot: '20:30', court: 'P3', homeFranchise: 'samurai-kicksmashers', awayFranchise: 'ice-breakers', homeNames: ['Muneer Shaik', 'Shaun Moropa'], awayNames: ['Jacques Van Zyl', 'Phil-Mar Van Rensburg'], sets: [[1,6],[3,6],[7,10]], games: [0,4], winner: 'away' }),
     ] },
   },
   {
     id: 'fx-w6-ice-lions', round: 6, league: 'mens', home: 'ice-breakers', away: 'sahara-lions',
     start: '2026-08-03T18:00:00+02:00', status: 'final', court: 'Padel 24',
     score: { winner: 'away', totals: [7, 15], rubberWins: [2, 4], rubbers: [
-      rubber({ slot: '18:00', court: 'P2', homeFranchise: 'ice-breakers', awayFranchise: 'sahara-lions', homeNames: ['Jacques', 'Zayd Methar'], awayNames: ['Naeem Omar', 'Alfaiz Mamji'], sets: [[3,6],[6,3],[8,10]], games: [0,3], winner: 'away' }),
-      rubber({ slot: '18:00', court: 'P3', homeFranchise: 'ice-breakers', awayFranchise: 'sahara-lions', homeNames: ['Irshaad', 'Aadam Nomani'], awayNames: ['Majid Bapu', 'Imtiaz Mohamed'], sets: [[6,3],[7,5],[5,10]], games: [3,0], winner: 'home' }),
+      rubber({ slot: '18:00', court: 'P2', homeFranchise: 'ice-breakers', awayFranchise: 'sahara-lions', homeNames: ['Jacques Van Zyl', 'Zayd Methar'], awayNames: ['Naeem Omar', 'Alfaiz Mamji'], sets: [[3,6],[6,3],[8,10]], games: [0,3], winner: 'away' }),
+      rubber({ slot: '18:00', court: 'P3', homeFranchise: 'ice-breakers', awayFranchise: 'sahara-lions', homeNames: ['Irshaad Moola', 'Aadam Nomani'], awayNames: ['Majid Bapu', 'Imtiaz Mohamed'], sets: [[6,3],[7,5],[5,10]], games: [3,0], winner: 'home' }),
       rubber({ slot: '19:15', court: 'P1', homeFranchise: 'ice-breakers', awayFranchise: 'sahara-lions', homeNames: ['Maaz Randera', 'Zaheer Methar'], awayNames: ['Pieter Badenhorst', 'Justin van Staaden'], sets: [[1,6],[3,6],[4,10]], games: [0,4], winner: 'away' }),
-      rubber({ slot: '19:15', court: 'P2', homeFranchise: 'ice-breakers', awayFranchise: 'sahara-lions', homeNames: ['Chaz Taylor', 'Nicki'], awayNames: ['Adil Patel', 'Soyab Patel'], sets: [[2,6],[6,7],[6,10]], games: [0,4], winner: 'away' }),
-      rubber({ slot: '20:30', court: 'P1', homeFranchise: 'ice-breakers', awayFranchise: 'sahara-lions', homeNames: ['Duhan Swart', 'JD'], awayNames: ['Cian Maritz', 'Yusuf Packery'], sets: [[3,6],[4,6],[9,11]], games: [0,4], winner: 'away' }),
+      rubber({ slot: '19:15', court: 'P2', homeFranchise: 'ice-breakers', awayFranchise: 'sahara-lions', homeNames: ['Chaz Taylor', 'Nicky Joubert'], awayNames: ['Adil Patel', 'Soyab Patel'], sets: [[2,6],[6,7],[6,10]], games: [0,4], winner: 'away' }),
+      rubber({ slot: '20:30', court: 'P1', homeFranchise: 'ice-breakers', awayFranchise: 'sahara-lions', homeNames: ['Duhan Swart', 'JD Herbst'], awayNames: ['Cian Maritz', 'Yusuf Packery'], sets: [[3,6],[4,6],[9,11]], games: [0,4], winner: 'away' }),
       rubber({ slot: '20:30', court: 'P3', homeFranchise: 'ice-breakers', awayFranchise: 'sahara-lions', homeNames: ['Wayne Enslin', 'Waldo van Tonder'], awayNames: ['Warren Morgan', 'Irfaan Mahomed'], sets: [[6,4],[7,6],[10,7]], games: [4,0], winner: 'home' }),
     ] },
   },
   {
-    id: 'fx-playoff-falcons-sonics', round: 7, stage: 'qualifier', league: 'mens', home: 'desert-falcons', away: 'sonic-viboras',
+    id: 'fx-playoff-falcons-sonics', round: 7, roundLabel: 'Final Qualifier', stage: 'qualifier', league: 'mens', home: 'desert-falcons', away: 'sonic-viboras',
     start: '2026-08-06T18:00:00+02:00', status: 'final', court: 'Padel 24',
     score: { winner: 'away', totals: [8, 15], rubberWins: [2, 4], rubbers: [
-      rubber({ slot: '18:00', court: 'P2', homeFranchise: 'desert-falcons', awayFranchise: 'sonic-viboras', homeNames: ['Ray Cassim', 'Ebrahim Mungalee'], awayNames: ['Marius Loock', 'Khalid Jeewa'], sets: [[5,7],[1,6],[8,10]], games: [0,4], winner: 'away' }),
-      rubber({ slot: '18:00', court: 'P3', homeFranchise: 'desert-falcons', awayFranchise: 'sonic-viboras', homeNames: ['Danie', 'Jude van den Berg'], awayNames: ['Gerrit Smith', 'Muhammad Dadamia'], sets: [[6,2],[6,4],[10,5]], games: [4,0], winner: 'home' }),
-      rubber({ slot: '19:15', court: 'P1', homeFranchise: 'desert-falcons', awayFranchise: 'sonic-viboras', homeNames: ['Reinhardt van Wyk', 'Schalk Schutte'], awayNames: ['Pieter Boshoff', 'Yusuf Moola'], sets: [[6,2],[6,2],[10,7]], games: [4,0], winner: 'home' }),
-      rubber({ slot: '19:15', court: 'P1', homeFranchise: 'desert-falcons', awayFranchise: 'sonic-viboras', homeNames: ['Uwaiz Patel', 'Yusuf Patel'], awayNames: ['Heinrich Coomans', 'Anton'], sets: [[7,5],[3,6],[6,10]], games: [0,3], winner: 'away' }),
+      rubber({ slot: '18:00', court: 'P2', homeFranchise: 'desert-falcons', awayFranchise: 'sonic-viboras', homeNames: ['Rayhaan Dinath', 'Ebrahim Mungalee'], awayNames: ['Marius Loock', 'Khalid Jeewa'], sets: [[5,7],[1,6],[8,10]], games: [0,4], winner: 'away' }),
+      rubber({ slot: '18:00', court: 'P3', homeFranchise: 'desert-falcons', awayFranchise: 'sonic-viboras', homeNames: ['Danie Rautenbach', 'Jude van den Berg'], awayNames: ['Gerrit Smith', 'Mohamed Dadamia'], sets: [[6,2],[6,4],[10,5]], games: [4,0], winner: 'home' }),
+      rubber({ slot: '19:15', court: 'P1', homeFranchise: 'desert-falcons', awayFranchise: 'sonic-viboras', homeNames: ['Reinhardt Trollip', 'Schalk Schutte'], awayNames: ['Pieter Boshoff', 'Yusuf Moola'], sets: [[6,2],[6,2],[10,7]], games: [4,0], winner: 'home' }),
+      rubber({ slot: '19:15', court: 'P1', homeFranchise: 'desert-falcons', awayFranchise: 'sonic-viboras', homeNames: ['Uwaiz Patel', 'Yusuf Patel'], awayNames: ['Heinrich Coomans', 'Anton Grote'], sets: [[7,5],[3,6],[6,10]], games: [0,3], winner: 'away' }),
       rubber({ slot: '20:30', court: 'P2', homeFranchise: 'desert-falcons', awayFranchise: 'sonic-viboras', homeNames: ['Warno Smit', 'Fahad Patel'], awayNames: ['Warwick Morgan', 'Joshua Hoffman'], sets: [[4,6],[4,6],[6,10]], games: [0,4], winner: 'away' }),
-      rubber({ slot: '20:30', court: 'P3', homeFranchise: 'desert-falcons', awayFranchise: 'sonic-viboras', homeNames: ['Jacques', 'Reino Grobler'], awayNames: ['George du Toit', 'Dewald Meyer'], sets: [[4,6],[4,6],[5,10]], games: [0,4], winner: 'away' }),
+      rubber({ slot: '20:30', court: 'P3', homeFranchise: 'desert-falcons', awayFranchise: 'sonic-viboras', homeNames: ['Jacques Burger', 'Reino Grobler'], awayNames: ['George du Toit', 'Dewald Meyer'], sets: [[4,6],[4,6],[5,10]], games: [0,4], winner: 'away' }),
+    ] },
+  },
+  {
+    id: 'fx-playoff-lions-falcons-semifinal', round: 8, roundLabel: 'Franchise League Semi-final', stage: 'semifinal', league: 'mens', home: 'sahara-lions', away: 'desert-falcons',
+    start: '2026-08-13T18:00:00+02:00', status: 'final', court: 'Padel 24',
+    score: { winner: 'away', totals: [11, 12], rubberWins: [3, 3], rubbers: [
+      rubber({ slot: '18:00', court: 'P3', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Drikus Prins', 'Warren Morgan'], awayNames: ['Jacques Burger', 'Reino Grobler'], sets: [[6,1],[0,6],[7,10]], games: [0,3], winner: 'away' }),
+      rubber({ slot: '18:00', court: 'P3', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Majid Bapu', 'Irfaan Mahomed'], awayNames: ['Danie Rautenbach', 'Jude van den Berg'], sets: [[3,6],[0,6],[7,10]], games: [0,4], winner: 'away' }),
+      rubber({ slot: '19:15', court: 'P1', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Cian Maritz', 'Yusuf Packery'], awayNames: ['Uwaiz Patel', 'Yusuf Patel'], sets: [[7,6],[1,6],[12,10]], games: [3,0], winner: 'home' }),
+      rubber({ slot: '19:15', court: 'P2', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Adil Patel', 'Soyab Patel'], awayNames: ['Fahad Patel', 'Rayhaan Dinath'], sets: [[6,1],[6,2],[11,9]], games: [4,0], winner: 'home' }),
+      rubber({ slot: '20:30', court: 'P1', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Pieter Badenhorst', 'Justin van Staaden'], awayNames: ['Reinhardt Trollip', 'Schalk Schutte'], sets: [[1,6],[3,6],[4,10]], games: [0,4], winner: 'away' }),
+      rubber({ slot: '20:30', court: 'P2', homeFranchise: 'sahara-lions', awayFranchise: 'desert-falcons', homeNames: ['Alfaiz Mamji', 'Naeem Omar'], awayNames: ['Warno Smit', 'Ryan Wicht'], sets: [[5,7],[6,3],[10,6]], games: [3,0], winner: 'home' }),
     ] },
   },
 ];
 
-const matchupKey = (fixture) => [fixture.home, fixture.away].sort().join('|');
+const matchupKey = (fixture) => `${[fixture.home, fixture.away].sort().join('|')}|${fixture.stage || 'regular'}|${fixture.round}`;
 const officialKeys = new Set(FINALS_RUN_FIXTURES.map(matchupKey));
 for (let index = FIXTURES.length - 1; index >= 0; index -= 1) {
   const fixture = FIXTURES[index];
@@ -108,7 +120,7 @@ const updatePlayer = (id, result, side, scoreIndex) => {
   player.stats.played += 1;
   player.stats.sets_won += setsWon;
   player.stats.sets_lost += setsLost;
-  player.stats.games_won += (result.sets[0]?.[scoreIndex] || 0) + (result.sets[1]?.[scoreIndex] || 0);
+  player.stats.games_won += result.sets.reduce((sum, setScore) => sum + (Number(setScore?.[scoreIndex]) || 0), 0);
   if (result.winner === side) {
     player.stats.wins += 1;
     player.stats.rubbers_won += 1;
@@ -121,7 +133,7 @@ const updatePlayer = (id, result, side, scoreIndex) => {
 
 FINALS_RUN_FIXTURES.forEach((fixture) => {
   fixture.score.rubbers.forEach((result) => {
-    if (fixture.stage !== 'qualifier') {
+    if (!fixture.stage) {
       applyTableResult(STANDINGS.mens.franchise, fixture, result);
       applyTableResult(STANDINGS.mens[result.court], fixture, result);
     }

50f20485ea1d285eae89f6bcd3630e52eb5c9ae8
amadadi78614 <mohamed.nomani@gmail.com>
2026-08-14 07:03:57 +0200

    Load verified Season 3 player rankings

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import './data/franchiseFinalsRunUpdate';
 import './data/regularSeasonSixRoundFix';
 import './data/mvpPairIntegrity';
 import './data/rankingRubberCorrections';
+import './data/verifiedSeason3PlayerStats';
 import './data/ladiesSeason2Data';
 import './data/ladiesSeason2HomeSync';
 import './data/ladiesSeason2LeagueSync';

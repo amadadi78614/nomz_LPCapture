35dd1e6df5f89f4dbc335a4c133c2bdfd52037a4
amadadi78614 <mohamed.nomani@gmail.com>
2026-08-22 09:06:33 +0200

    Wire full-detail Legacy League standings table

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,7 @@ import './data/ladiesSeason2LeagueSync';
 import './data/ladiesSeason2Round1Update';
 import './data/ladiesSeason2RankingsSync';
 import './data/superCupSquadSync';
+import './data/legacyDetailedTableSync';
 import './styles/global.css';
 import './styles/career-table-mobile.css';
 import './styles/home-premium.css';
